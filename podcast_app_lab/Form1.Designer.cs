21cd39c5b1844e560f29aeb5680a97bdc4de1b38
tt2357 <ttymek123@gmail.com>
2023-06-05 00:14:12 +0200

    .

--- podcast_app_lab/Form1.Designer.cs
+++ podcast_app_lab/Form1.Designer.cs
@@ -33,17 +33,19 @@
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // listView1
             // 
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(12, 3);
+            this.listView1.Location = new System.Drawing.Point(9, 2);
+            this.listView1.Margin = new System.Windows.Forms.Padding(2);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(337, 389);
+            this.listView1.Size = new System.Drawing.Size(254, 317);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.List;
@@ -51,9 +53,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 398);
+            this.button1.Location = new System.Drawing.Point(9, 323);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(66, 40);
+            this.button1.Size = new System.Drawing.Size(50, 32);
             this.button1.TabIndex = 1;
             this.button1.Text = "+";
             this.button1.UseVisualStyleBackColor = true;
@@ -61,9 +64,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(283, 398);
+            this.button2.Location = new System.Drawing.Point(212, 323);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(66, 40);
+            this.button2.Size = new System.Drawing.Size(50, 32);
             this.button2.TabIndex = 2;
             this.button2.Text = "-";
             this.button2.UseVisualStyleBackColor = true;
@@ -73,61 +77,80 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(458, 59);
+            this.label1.Location = new System.Drawing.Point(326, 92);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(95, 36);
+            this.label1.Size = new System.Drawing.Size(79, 29);
             this.label1.TabIndex = 3;
             this.label1.Text = "label1";
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(369, 103);
+            this.button4.Location = new System.Drawing.Point(331, 136);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(54, 50);
+            this.button4.Size = new System.Drawing.Size(40, 41);
             this.button4.TabIndex = 5;
             this.button4.Text = "▷";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(473, 103);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(54, 50);
-            this.button5.TabIndex = 6;
-            this.button5.Text = "I I";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(571, 103);
+            this.button6.Location = new System.Drawing.Point(482, 136);
+            this.button6.Margin = new System.Windows.Forms.Padding(2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(54, 50);
+            this.button6.Size = new System.Drawing.Size(40, 41);
             this.button6.TabIndex = 7;
             this.button6.Text = "□";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(369, 181);
+            this.progressBar1.Location = new System.Drawing.Point(331, 199);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(2);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(256, 27);
+            this.progressBar1.Size = new System.Drawing.Size(192, 22);
             this.progressBar1.TabIndex = 8;
             this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(443, 92);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 29);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(418, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 29);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "/";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listView1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -143,9 +166,10 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- podcast_app_lab/Form1.cs
+++ podcast_app_lab/Form1.cs
@@ -16,25 +16,38 @@ namespace podcast_app_lab
     public partial class Form1 : Form
     {
         Timer timer1 = new Timer();
-        private int start = 0;
-        private string nazwapliku;
-        private double mduration;
+        private string fileName;
+        private double mediaDuration;
         OpenFileDialog fd = new OpenFileDialog();
         WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
+        bool isMediaEnded = true;
+        bool isPaused = false;
+        double pausedPosition = 0;
+
         public Form1()
         {
             InitializeComponent();
+
+            timer1.Interval = 1000;
+            timer1.Tick += new EventHandler(timer1_Tick);
+            wplayer.PlayStateChange += new WMPLib._WMPOCXEvents_PlayStateChangeEventHandler(wplayer_PlayStateChange);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            fd.ShowDialog();
-            string filename = fd.FileName;
-            listView1.Items.Add(filename);
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                fileName = fd.FileName;
+                listView1.Items.Add(fileName);
+            }
         }
+
         private void button2_Click(object sender, EventArgs e)
         {
-            listView1.SelectedItems[0].Remove();
+            if (listView1.SelectedItems.Count > 0)
+            {
+                listView1.SelectedItems[0].Remove();
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -43,23 +56,39 @@ namespace podcast_app_lab
             listView1.Columns[0].Width = 300;
         }
 
-        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-        }
-
         private void button4_Click(object sender, EventArgs e)
         {
             if (listView1.SelectedItems.Count > 0)
             {
-                nazwapliku = listView1.SelectedItems[0].Text;
-            }
-            wplayer.URL = nazwapliku;
-            wplayer.controls.play();
-            mduration = wplayer.currentMedia.duration;
+                
 
-            timer1.Tick += new EventHandler(timer1_Tick);
-            label1.Text = start.ToString();
-            timer1.Start();
+                if (wplayer.playState == WMPLib.WMPPlayState.wmppsPlaying)
+                {
+                    button4.Text = "▷";
+                    wplayer.controls.pause();
+                    pausedPosition = wplayer.controls.currentPosition;
+                    isPaused = true;
+                }
+                else if (wplayer.playState == WMPLib.WMPPlayState.wmppsPaused || wplayer.playState == WMPLib.WMPPlayState.wmppsStopped)
+                {
+                    button4.Text = "||";
+                    wplayer.controls.currentPosition = pausedPosition;
+                    wplayer.controls.play();
+
+                    if (!isPaused)
+                    {
+                        mediaDuration = wplayer.currentMedia.duration;
+                        label2.Text = TimeSpan.FromSeconds(mediaDuration).ToString(@"hh\:mm\:ss");
+                        timer1.Start();
+                    }
+                }
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            fileName = listView1.SelectedItems[0].Text;
+            wplayer.URL = fileName;
         }
 
         private void progressBar1_Click(object sender, EventArgs e)
@@ -69,13 +98,54 @@ namespace podcast_app_lab
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            start++;
-            label1.Text = start.ToString() + "/" + mduration.ToString();
+            if (wplayer.playState == WMPLib.WMPPlayState.wmppsPlaying)
+            {
+                label1.Text = TimeSpan.FromSeconds(wplayer.controls.currentPosition).ToString(@"hh\:mm\:ss");
 
-            if (mduration == start)
+                double progressPercentage = (wplayer.controls.currentPosition / mediaDuration) * 100;
+                progressBar1.Value = Math.Min((int)progressPercentage, 100);
+            }
+            else if (wplayer.playState == WMPLib.WMPPlayState.wmppsStopped)
             {
                 timer1.Stop();
+                progressBar1.Value = 0;
             }
+        }
+
+        private void wplayer_PlayStateChange(int newState)
+        {
+            if ((WMPLib.WMPPlayState)newState == WMPLib.WMPPlayState.wmppsStopped)
+            {
+                isMediaEnded = true;
+                pausedPosition = 0;
+            }
+
+            if ((WMPLib.WMPPlayState)newState == WMPLib.WMPPlayState.wmppsPlaying && isMediaEnded)
+            {
+                isMediaEnded = false;
+                if (isPaused)
+                {
+                    wplayer.controls.currentPosition = pausedPosition;
+                }
+                else
+                {
+                    mediaDuration = wplayer.currentMedia.duration;
+                    label2.Text = TimeSpan.FromSeconds(mediaDuration).ToString(@"hh\:mm\:ss");
+                    timer1.Start();
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            wplayer.controls.stop();
+            timer1.Stop();
+            progressBar1.Value = 0;
+            isPaused = false;
         }
     }
 }
